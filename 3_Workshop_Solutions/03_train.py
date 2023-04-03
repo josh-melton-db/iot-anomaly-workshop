ec0564c67f5316a434d91e3f5cde278639292135
josh.melton@databricks.com <josh.melton@databricks.com>
2023-04-03 21:04:40 +0000

    Added licensing info

--- 3_Workshop_Solutions/03_train.py
+++ 3_Workshop_Solutions/03_train.py
@@ -197,3 +197,11 @@ client.transition_model_version_stage(
   stage="Production",
   archive_existing_versions=True
 )
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC &copy; 2023 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/>
+# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="https://help.databricks.com/">Support</a>
