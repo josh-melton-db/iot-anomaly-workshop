080e42a14a5b72c8ceb6ad6001f6d384dd220d9b
josh.melton@databricks.com <josh.melton@databricks.com>
2023-04-04 15:49:36 +0000

    updated agenda, removed readme

--- Agenda.py
+++ Agenda.py
@@ -1,6 +1,30 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC 
+# MAGIC Real-time monitoring and anomaly detection on streaming IoT pipelines in Manufacturing The Internet of Things (IoT) is a foundational technology that enables the delivery of Industry 4.0 objectives within manufacturing and connected products. IoT data provides critical insights into manufacturing processes and product performance, enabling companies to optimize operations, improve quality, and deliver innovative products and services to customers.
+# MAGIC </br></br>
+# MAGIC In this solution accelerator, we show how to build a streaming pipeline for IoT data, train a machine learning model on that data, and use that model to make predictions on new IoT data. The pattern is applicable across many use cases like
+# MAGIC 
+# MAGIC - predictive maintenance
+# MAGIC - digital twins
+# MAGIC - remote monitoring
+# MAGIC - asset optimization
+# MAGIC - energy consumption optimization
+# MAGIC - workforce optimization and safety
+# MAGIC - quality control 
+# MAGIC </br></br>
+# MAGIC Databricks Lakehouse overcomes the limitations of legacy platforms for dealing with IoT data. In this workshop you will:
+# MAGIC - Ingest data from cloud storage 
+# MAGIC - Load and transform IoT streaming data into Delta 
+# MAGIC - Surface insights like anomalies on streamed signals in near real-time
+# MAGIC </br></br>
+# MAGIC Getting started </br>
+# MAGIC Although specific solutions can be downloaded as .dbc archives from our websites, we recommend cloning these repositories onto your databricks environment. Not only will you get access to latest code, but you will be part of a community of experts driving industry best practices and re-usable solutions, influencing our respective industries. To start, simply clone solution accelerator repository in Databricks using Databricks Repos
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
 # MAGIC | Time | Lesson &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; | Description &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; |
 # MAGIC |:----:|-------|-------------|
 # MAGIC | 60m  | **Slides**                               | *Introductions, concepts overview* |
