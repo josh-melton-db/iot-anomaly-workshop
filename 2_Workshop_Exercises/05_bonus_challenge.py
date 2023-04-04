56c886557c10f4e6b817ca3e18b2c8603ff59882
josh.melton@databricks.com <josh.melton@databricks.com>
2023-04-04 13:43:55 +0000

    Added note about hyperopt

--- 2_Workshop_Exercises/05_bonus_challenge.py
+++ 2_Workshop_Exercises/05_bonus_challenge.py
@@ -4,6 +4,11 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC If you're curious about machine learning, check out solution notebok number 3 - there's an extra couple of cells that uses Hyperopt for hyperparameter tuning
+
+# COMMAND ----------
+
 # MAGIC %md-sandbox
 # MAGIC &copy; 2023 Databricks, Inc. All rights reserved.<br/>
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
